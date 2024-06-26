e34c510b4aa488c293f48be12dd9cd0d49af51ff
SriMoorthy <srimoorthy27@gmail.com>
2024-06-26 13:53:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function makeid(l) {
   // write your code here
+	let S = ''
+	let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+	for (let i = 0; i < l; i++) {
+		const r = parseInt(Math.random()*str.length)
+		S+=str[r]
+	}
+	return S
 }
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
-alert(makeid(l));
+// const l = prompt("Enter a number.");
+// alert(makeid(l));
